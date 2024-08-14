6c9973e23a5c54afad5fd3c3765651c57aee4e2f
AnikaTahsin06 <bushratahsin653@gmail.com>
2024-08-14 23:31:19 +0600

    implemented basic styles and signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './styles/App.css'
+import Signup from './pages/Signup/Signup';
+import Signin from './pages/Signin/Signin';
+import Feed from './pages/Feed/Feed';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 
 function App() {
 
   return (
-    <>
-      <h1>Welcom to meet max app!!!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/" element={<Feed />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/Signin/Signin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Signin.css'
+
+const Signin = () => {
+  return (
+    <div className='container sign__in'>Signin</div>
+  )
+}
+
+export default Signin
