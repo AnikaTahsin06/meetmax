abc40a9eb4d3f893b735baa86aa9c0795f5ea9d1
AnikaTahsin06 <bushratahsin653@gmail.com>
2024-08-20 21:20:48 +0600

    fixed styles issues

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -8,7 +8,7 @@ import explore from '../../assets/icons/Explore.png'
 import profile from '../../assets/icons/User.png'
 import settings from '../../assets/icons/Setting.png'
 import logout from '../../assets/icons/LogOut.png'
-import './LeftSidebar.css';  
+import './LeftSidebar.css';
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
@@ -22,33 +22,44 @@ const LeftSidebar = () => {
   return (
     <div className="left-menu">
       <Link to="/" className="sidebar-item active">
-        <span className="icon"><img src={homeWhiteIcon} alt="Home Icon"/></span><span className="text">Feed</span>
+        <span className="icon"><img src={homeWhiteIcon} alt="Home Icon" /></span><span className="text">Feed</span>
       </Link>
       <Link to="/community" className="sidebar-item">
-        <span className="icon"><img src={community} alt="Community Icon"/></span><span className="text">My community</span>
+        <span className="icon"><img src={community} alt="Community Icon" /></span><span className="text">My community</span>
       </Link>
-      <Link to="/messages" className="sidebar-item">
-        <span className="icon"><img src={message} alt="message Icon"/></span><span className="text">Messages</span>
-      </Link>
-      <Link to="/notifications" className="sidebar-item">
-        <span className="icon"><img src={notification} alt="notification Icon"/></span><span className="text">Notification</span>
-        <span className="notification-badge">2</span>  
-      </Link>
+      <div className=' no-visible-tab'>
+        <Link to="/messages" className="sidebar-item">
+          <span className="icon"><img src={message} alt="message Icon" /></span><span className="text">Messages</span>
+        </Link>
+      </div>
+      <div className='no-visible-tab'>
+        <Link to="/notifications" className="sidebar-item">
+          <span className="icon"><img src={notification} alt="notification Icon" /></span><span className="text">Notification</span>
+          <span className="notification-badge">2</span>
+        </Link>
+      </div>
       <Link to="/explore" className="sidebar-item">
-        <span className="icon"><img src={explore} alt="explore Icon"/></span><span className="text">Explore</span>
+        <span className="icon"><img src={explore} alt="explore Icon" /></span><span className="text">Explore</span>
       </Link>
-      <Link to="/profile" className="sidebar-item">
-        <span className="icon"><img src={profile} alt="profile Icon"/></span><span className="text">Profile</span>
+      <div className=' no-visible-laptop'>
+        <Link to="/notifications" className="sidebar-item">
+          <div>
+            <span className="icon"><img src={notification} alt="notification Icon" /></span><span className="text">Notification</span>
+            <span className="notification-badge">2</span>
+          </div>
+        </Link>
+      </div>
+      <Link to="/profile" className="sidebar-item no-visible-tab">
+        <span className="icon"><img src={profile} alt="profile Icon" /></span><span className="text">Profile</span>
       </Link>
       <Link to="/settings" className="sidebar-item">
-        <span className="icon"><img src={settings} alt="settings Icon"/></span><span className="text">Settings</span>
+        <span className="icon"><img src={settings} alt="settings Icon" /></span><span className="text">Settings</span>
       </Link>
-      {/* <Link onClick={handleLogout} className="sidebar-item">
-        <span className="icon"><img src={logout} alt="logout Icon"/></span><span className="text">Logout</span>
-      </Link> */}
-      <button onClick={handleLogout} className="sidebar-item logout-button">
-        <span className="icon"><img src={logout} alt="Logout Icon"/></span><span className="text">Logout</span>
-      </button>
+      <div className=' no-visible-tab'>
+        <button onClick={handleLogout} className="sidebar-item logout-button">
+          <span className="icon"><img src={logout} alt="Logout Icon" /></span><span className="text">Logout</span>
+        </button>
+      </div>
     </div>
   );
 }
